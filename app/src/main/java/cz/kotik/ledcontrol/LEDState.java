020563dc63065d2713f97f2bb289148b14bba3c7
DESKTOP-OU5SJ6K\Kotik <xkotas07@stud.fit.vutbr.cz>
2017-11-23 17:54:07 +0100

    Vytvoreni projektu + zakladni GUI

---
+++ app/src/main/java/cz/kotik/ledcontrol/LEDState.java
@@ -0,0 +1,13 @@
+package cz.kotik.ledcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LEDState extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ledstate);
+    }
+}
